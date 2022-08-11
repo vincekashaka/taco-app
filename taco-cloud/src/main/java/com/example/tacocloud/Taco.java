78c094ef29db77c188b36368a88714a3e1aff7d8
Vincent Kashaka <vincekashaka@gmail.com>
2022-08-11 12:43:03 +0200

    first commit

---
+++ taco-cloud/src/main/java/com/example/tacocloud/Taco.java
@@ -0,0 +1,14 @@
+package com.example.tacocloud;
+
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class Taco {
+
+    private String name;
+
+    private List<Ingredient> ingredients;
+}
